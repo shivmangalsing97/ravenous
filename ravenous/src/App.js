925c508d4f47258b749b8ab890709375ae641283
shivmangalsing97 <shivmangalsing97@gmail.com>
2019-06-18 16:59:30 +0530

    second

--- ravenous/src/App.js
+++ ravenous/src/App.js
@@ -1,16 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import BussinessList from './components/BusinessList/BussinessList' ;
 import SearchBar from './components/SearchBar/SearchBar' ;
+//import Business from './components/Business/Business';
+
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+name: 'MarginOtto Pizzeria',
+address: '1010 Paddington Way',
+city: 'Flavortown',
+state: 'NY',
+zipCode: '10101',
+category: 'Italian',
+rating: 4.5,
+reviewCount: 90
+}
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+]
 
 class App extends React.Component {
+  searchYelp(term,location,sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  }
+  
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
-        <BussinessList /> 
+        <SearchBar searchYelp={this.searchYelp} />
+        <BussinessList businesses={businesses} /> 
     </div>
     );
   }
--- ravenous/src/components/Business/Business.js
+++ ravenous/src/components/Business/Business.js
@@ -1,35 +1,25 @@
 import React from 'react' ;
 import './Business.css' ;
 
-const Businesses = {
-    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-}
+
 class Business extends React.Component {
     render() {
         return (
             <div className="Business">
                 <div className="image-container">
-                    <img src= {Businesses.imageSrc}/>
+                    <img src= {this.props.business.imageSrc} alt=''  />
                 </div>
-                <h2>{Businesses.name}</h2>
+                <h2>{this.props.business.name}</h2>
                 <div className="Business-information">
                     <div className="Business-address">
-                        <p>{Businesses.address}</p>
-                        <p>{Businesses.city}</p>
-                        <p>{Businesses.state}, {Businesses.zipCode}</p>
+                        <p>{this.props.business.address}</p>
+                        <p>{this.props.business.city}</p>
+                        <p>{this.props.business.state}, {this.props.business.zipCode}</p>
                     </div>
                     <div className="Business-reviews">
-                        <h3>{Businesses.category}</h3>
-                        <h3 className="rating">{Businesses.rating}</h3>
-                        <p>{Businesses.reviewCount}</p>
+                        <h3>{this.props.business.category}</h3>
+                        <h3 className="rating">{this.props.business.rating}</h3>
+                        <p>{this.props.business.address.reviewCount}</p>
                     </div>
                 </div>
             </div>
